97df75581087e498cbd87be7e045257c27ca68e0
Alex Bechmann <alex.bechmann@outlook.com>
2018-05-28 08:53:01 +0200

    prettier

--- core/src/parse-sdk/init-parse.ts
+++ core/src/parse-sdk/init-parse.ts
@@ -2,5 +2,5 @@ export const initParse = (parse: any, parseConfig: any) => {
   [Parse, parse].forEach(p => {
     p.initialize(parseConfig.applicationId, parseConfig.javaScriptKey);
     p.serverURL = parseConfig.serverUrl;
-  })
+  });
 };
